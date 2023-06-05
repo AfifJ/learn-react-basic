e089746f91211a4aad79fca17b694cced5d1ac42
AfifJ <110212622+AfifJ@users.noreply.github.com>
2023-06-05 18:21:42 +0700

    little update

--- src/components/shoppingPage/ShoppingPage.jsx
+++ src/components/shoppingPage/ShoppingPage.jsx
@@ -5,6 +5,7 @@ const ShoppingPage = () => {
     { id: 1, name: "Product 1", price: 9.99 },
     { id: 2, name: "Product 2", price: 19.99 },
     { id: 3, name: "Product 3", price: 14.99 },
+    { id: 3, name: "Product 4", price: 14.99 },
   ]);
 
   const handleAddToCart = (productId) => {
